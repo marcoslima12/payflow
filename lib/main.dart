9d1049a037ba3efca851f34988863234d11096c2
marcoslima12 <marcosantonio-10@live.com>
2024-06-16 14:29:24 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:payflow/modules/splash/splash_page.dart';
+import 'package:payflow/shared/themes/app_colors.dart';
+
+void main() {
+  runApp(const AppWidget());
+}
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(primaryColor: AppColors.primary),
+        home: const SplashPage());
+  }
+}
