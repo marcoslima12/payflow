66fd396f166a7e69f43bc6dc0d4818d8590f2ee8
marcoslima12 <marcosantonio-10@live.com>
2024-06-19 09:37:40 -0300

    add firebase

---
+++ lib/app_widget.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+import 'modules/login/login_page.dart';
+import 'shared/themes/app_colors.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(primaryColor: AppColors.primary),
+        home: const LoginPage());
+  }
+}
