c7cbebccf199e2c42b5e7510c3eafd012bd38d73
marcoslima12 <marcosantonio-10@live.com>
2024-06-25 20:33:07 -0300

    fix google credentials

--- lib/modules/login/login_controller.dart
+++ lib/modules/login/login_controller.dart
@@ -1,5 +1,4 @@
 // ignore_for_file: use_build_context_synchronously
-import 'dart:developer';
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -15,12 +14,11 @@ class LoginController {
       GoogleAuthProvider googleAuthProvider = GoogleAuthProvider();
       final response = await _auth.signInWithProvider(googleAuthProvider);
       final user = response.user;
-      final loggedUser =
-          UserModel(name: "Nome do Usuário", photoURL: user?.photoURL);
+      print(user);
+      final loggedUser = UserModel(name: user!.email!, photoURL: user.photoURL);
       authController.setUser(context, loggedUser);
     } catch (error) {
       authController.setUser(context, null);
-      print('Erro: $error');
     }
   }
 }
